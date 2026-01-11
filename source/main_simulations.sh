b140566b8ecfac0c22b8df0553dd7f622ebe3998
Weijia Qian <ainibuni@gmail.com>
2026-01-10 22:42:51 -0500

    main simulations

---
+++ source/main_simulations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --array=1-18%18
+#SBATCH --job-name=main_simulations
+#SBATCH --partition=wrobel
+#SBATCH --output=main_simulations.out
+#SBATCH --error=main_simulations.err
+
+module purge
+module load R/4.4.0
+
+# Rscript to run an r script
+# This stores which job is running (1, 2, 3, etc)
+JOBID=$SLURM_ARRAY_TASK_ID
+Rscript main_simulations.R $JOBID
+
+
